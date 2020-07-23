1ea11006adab02cf3d8b071230fdd0e2e60a0cfa
xiaowo <349468151@qq.com>
2020-07-23 17:52:51 +0800

    helloWorld

--- AlienGame/settings.py
+++ AlienGame/settings.py
@@ -3,9 +3,13 @@ class Settings(object):
 
     def __init__(self):
         # 设置背景色
-        self.bg_color = (230, 230, 230)
+        self.bg_color = 230, 230, 230
         # 设置屏幕大小
         self.screen_height = 600
         self.screen_width = 800
         # 飞船的设置
         self.ship_speed_factor = 1.5
+        self.bullet_speed_factor = 1
+        self.bullet_wide = 3
+        self.bullet_hight = 10
+        self.bullen_color = 60, 60, 60
