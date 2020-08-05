14f6143a4a9ce1c30b7f28a6d3be86c85184ae52
xiaowo <349468151@qq.com>
2020-08-05 22:55:05 +0800

    xiaowo

--- AlienGame/game_functions.py
+++ AlienGame/game_functions.py
@@ -184,8 +184,8 @@ def ship_hit(ai_settings, stats, screen, sb, ship, aliens, bullets):
         # 创建一群新的外星人，并将飞船放到屏幕底端中央
         create_fleet(ai_settings, screen, ship, aliens)
         ship.center_ship()
-        # 暂停
-        sleep(0.2)
+        # # 暂停
+        # sleep(0.2)
     else:
         stats.game_active = False
         pygame.mouse.set_visible(True)
