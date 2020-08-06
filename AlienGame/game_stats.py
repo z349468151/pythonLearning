bc3b74ab5b244325f6b59d83203934c8c8c2eb69
xiaowo <349468151@qq.com>
2020-08-06 22:56:34 +0800

    xiaowo

--- AlienGame/game_stats.py
+++ AlienGame/game_stats.py
@@ -11,7 +11,7 @@ class GameStats(object):
         self.high_score = 0
 
     def reset_stats(self):
-        """初始化在游戏运行期间可能变化的统计信息"""
+        """初始化在游戏运行期间可能变化的统计信息""" 
         self.ships_left = self.ai_settings.ship_limit
         self.score = 0
         self.level = 1
