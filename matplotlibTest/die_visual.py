961c757121082c66a8aad28398562b301694d179
xiaowo <349468151@qq.com>
2020-08-10 10:38:21 +0800

    helloWorld

---
+++ matplotlibTest/die_visual.py
@@ -0,0 +1,26 @@
+from matplotlibTest.die import Die
+import pygal
+
+# 创建一个D6
+die_1 = Die(8)
+die_2 = Die(8)
+
+# 掷几次骰子，并将结果存储在一个列表中
+results = []
+for roll in range(1000000):
+    result = die_1.roll() + die_2.roll()
+    results.append(result)
+# 分析结果
+frequencies = []
+for value in range(2, die_1.num_sides + die_2.num_sides + 1):
+    frequency = results.count(value)
+    frequencies.append(frequency)
+
+# 对结果进行可视化
+hist = pygal.Bar()
+hist.title = "Result of rolling one D6 1000 times."
+hist.x_labels = ['2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16']
+hist.x_title = "Result"
+hist.y_title = "Frequency of Result"
+hist.add('D8+D8', frequencies)
+hist.render_to_file('die_visual.svg')
